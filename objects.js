970d22497a61c1a75dd16d9018454be2cceb383e
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-01-17 23:52:19 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+ var playlist = { lone : 'chacka' }
+
+function updatePlaylist(playlist, artistName, songTitle){
+//  var playlist['Slowdive'] = 'Alison'
+//  var playlist['My Bloody Valentine'] = 'Sometimes'
+var playlist = object.assign({},{playlist},{Slowdive: Alison},{['My Bloody Valentine']: Sometimes})
+  var playlist = object.assign({},{playlist},{artistName: songTitle})
+
+return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+
+}
