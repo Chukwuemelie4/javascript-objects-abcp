9a125dcd1a607a521a160d49f11c8fdd7403d7f7
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-01-18 00:39:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,11 +3,11 @@
 function updatePlaylist(playlist, artistName, songTitle){
   playlist['Slowdive']= 'Alison'
   playlist['My Bloody Valentine']= 'Sometimes'
-  playlist = Object.assign({},{playlist},{ [artistName]: songTitle })
-
+  //playlist = Object.assign({},{playlist},{ [artistName]: songTitle })
+playlist[artistName]= songTitle 
 return playlist
 }
-//updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
 function removeFromPlaylist(playlist, artistName){
 return delete playlist.artistName
