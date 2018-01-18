10d0ecdb32d2f1a1f31e8b5aff891dc839da527e
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-01-18 00:19:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,9 @@
  var playlist = { lone : 'chacka' }
 
 function updatePlaylist(playlist, artistName, songTitle){
-//  var playlist['Slowdive'] = 'Alison'
-//  var playlist['My Bloody Valentine'] = 'Sometimes'
-var playlist = object.assign({},{playlist},{Slowdive: Alison},{['My Bloody Valentine']: Sometimes})
-  var playlist = object.assign({},{playlist},{artistName: songTitle})
+  playlist['Slowdive']= 'Alison'
+  playlist['My Bloody Valentine']= 'Sometimes'
+  var playlist = Object.assign({},{playlist},{artistName: songTitle})
 
 return playlist
 }
