d6068aa34aeb464a9f33dace6872e86ed37c7ed3
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-01-18 00:41:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,5 +10,5 @@ return playlist
 //updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
 
 function removeFromPlaylist(playlist, artistName){
-return delete playlist.[artistName]
+return delete playlist.artistName
 }
